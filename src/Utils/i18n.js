096fe42136672a9cc816dc6b1f2ff0a16973c9e3
Ahmet Özışık <ozisikahmet@gmail.com>
2019-10-04 15:07:45 +0300

    fix locale fallback

--- src/Utils/i18n.js
+++ src/Utils/i18n.js
@@ -4,13 +4,14 @@ import en from "./locales/en";
 import lo from "./locales/lo";
 import uk from "./locales/uk";
 
-I18n.defaultLocale = "en";
-
 const locales = RNLocalize.getLocales();
 
 if (Array.isArray(locales)) {
   I18n.locale = locales[0].languageCode;
 }
+
+I18n.defaultLocale = "en";
+I18n.fallbacks = I18n.defaultLocale;
 
 I18n.translations = {
   en,
